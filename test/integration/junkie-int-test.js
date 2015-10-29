f64beb6bc512b315e864cc0914558dce309db1d4
Troy Kinsella <troy.kinsella@gmail.com>
2015-10-29 00:42:56 -0700

    More tests.

--- test/integration/junkie-int-test.js
+++ test/integration/junkie-int-test.js
@@ -451,6 +451,18 @@ describe("junkie integration", function() {
       result.field.should.equal(D);
     });
 
+    it("should fail multiple creator injectors", function() {
+      var c = junkie.newContainer();
+
+      c.register("A", A)
+        .inject("B").into.constructor()
+        .inject("C").into.factory();
+
+      expect(function() {
+        c.resolve("A");
+      }).to.throw(Error, "Multiple creator injectors");
+    });
+
   });
 
   describe("two deps", function() {
