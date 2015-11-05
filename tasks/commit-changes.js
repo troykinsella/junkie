d144b082f583d2970011a54ff03b4aef7b423f2b
Troy Kinsella <troy.kinsella@gmail.com>
2015-11-04 22:47:59 -0800

    fix(release): Fixed release tag using previous version.

--- tasks/commit-changes.js
+++ tasks/commit-changes.js
@@ -5,7 +5,7 @@ var git = require('gulp-git');
 var commitChangesTask = function() {
   return gulp.src('.')
     .pipe(git.add())
-    .pipe(git.commit('[Prerelease] Bumped version number'));
+    .pipe(git.commit('chore(release): Release changes'));
 };
 
 gulp.task('commit-changes', commitChangesTask);
--- tasks/tag-release.js
+++ tasks/tag-release.js
@@ -2,9 +2,15 @@
 var gulp = require('gulp');
 var git = require('gulp-git');
 
+function getPackageJsonVersion() {
+  // We parse the json file instead of using require because require caches
+  // multiple calls so the version number won't be updated
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
+}
+
 var tagReleaseTask = function(cb) {
-  var version = require('../package.json').version;
-  git.tag(version, 'chore(release): Tagged ' + version, function(error) {
+  var version = getPackageJsonVersion();
+  git.tag("v" + version, 'chore(release): Tagged ' + version, function(error) {
     if (error) {
       return cb(error);
     }
